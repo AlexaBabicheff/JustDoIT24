e6502041c21d65d7f5dcf63eeb54e6db2b0ef56d
Skoryk Anna <annsskorik@gmail.com>
2024-04-29 20:31:14 +0200

    help me please with Forms

---
+++ my-react-app/src/components/utils/requests.js
@@ -0,0 +1,37 @@
+
+export const fetchUsers = async (setUserList) => {
+    try {
+      const response = await fetch('http://127.0.0.1:3333/sale')
+      if (!response.ok) throw new Error('не получилось забрать пользователей')
+      const data = await response.json()
+      setUserList(data.sale)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
+  export const fetchSingleUser = async (id, setUser) => {
+    const response = await fetch(`http://127.0.0.1:3333/sale/${id}`)
+    const data = await response.json()
+    console.log(data)
+    setUser(data)
+  }
+  
+  // export async await
+  // body передаем объект - параметр функции
+  // результат/ответ вывводим в консоль
+  export const addUser = async (newUser) => {
+    try {
+      const response = await fetch('http://127.0.0.1:3333/sale/send', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(newUser),
+      })
+      if (!response.ok) throw new Error('не получилось добавить пользователя')
+      const data = await response.json()
+      return data
+    } catch (error) {
+      console.log(error)
+    }
+  }
+ 
